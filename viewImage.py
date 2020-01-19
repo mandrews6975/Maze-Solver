071ce3e1dd4f8b23e73c80ecc650f4f1bc3df713
Michael Andrews <43254509+mandrews6975@users.noreply.github.com>
2020-01-18 19:11:14 -0600

    Added automated solved image viewing.

--- viewImage.py
+++ viewImage.py
@@ -1,13 +1,4 @@
-import eel, platform, sys
+import sys
+from PIL import Image
 
-eel.init("web")
-
-eel.loadImage(sys.argv[0])
-
-try:
-    eel.start("main.html", mode="chrome", size=(1000, 1000))
-except EnvironmentError:
-    if sys.platform in ["win32", "win64"] and int(platform.release()) >= 10:
-        eel.start("main.html", mode="edge", size=(1000, 1000))
-    else:
-        raise
+img = Image.open(sys.argv[1]).show()
--- web/js/main.js
+++
@@ -1,4 +0,0 @@
-eel.expose(loadImage);
-function loadImage(filename){
-  document.getElementById('img_maze').src = filename;
-}
